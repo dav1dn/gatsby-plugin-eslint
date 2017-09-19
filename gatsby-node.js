f383670fce7050cc6604fd2cd89528b43880f321
David Nguyen <d_nguyen@berkeley.edu>
2017-09-18 17:42:01 -0700

    initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,19 @@
+exports.modifyWebpackConfig = ({ config, stage }, options) => {
+  const jsFiles = /\.jsx?$/;
+  const eslintLoader = `eslint-loader?${JSON.stringify(options)}`;
+
+  switch (stage) {
+    case `develop`:
+      {
+        config.loader('eslint-loader', {
+          test: jsFiles,
+          loaders: [`style`, eslintLoader]
+        });
+
+        return config;
+      }
+
+    default:
+      return config;
+  }
+};
