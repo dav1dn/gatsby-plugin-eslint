2515cb6eb8414bc6324dea148ef5deddd32fdab0
David Nguyen <d_nguyen@berkeley.edu>
2017-09-18 17:57:07 -0700

    add noop index.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,25 @@
 exports.modifyWebpackConfig = ({ config, stage }, options) => {
   const jsFiles = /\.jsx?$/;
-  const eslintLoader = `eslint-loader?${JSON.stringify(options)}`;
+  const eslintLoader = `eslint-loader`;
 
+  console.log(options);
   switch (stage) {
     case `develop`:
       {
         config.loader('eslint-loader', {
           test: jsFiles,
-          loaders: [`style`, eslintLoader]
+          loader: eslintLoader,
+          query: {
+            failOnError: false
+          }
         });
 
         return config;
       }
 
     default:
-      return config;
+      {
+        return config;
+      }
   }
 };
--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,18 +1,23 @@
 exports.modifyWebpackConfig = ({ config, stage }, options) => {
   const jsFiles = /\.jsx?$/
-  const eslintLoader = `eslint-loader?${JSON.stringify(options)}`
+  const eslintLoader = `eslint-loader`
 
+  console.log(options)
   switch (stage) {
     case `develop`: {
       config.loader('eslint-loader', {
         test: jsFiles,
-        loaders: [`style`, eslintLoader],
+        loader: eslintLoader,
+        query: {
+          failOnError: false,
+        }
       })
 
       return config
     }
 
-    default:
+    default: {
       return config
+    }
   }
 }
